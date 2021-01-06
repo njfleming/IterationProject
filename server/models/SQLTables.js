0ec0a3d733213f5f3999b3ee676a28f73bc0c24b
Cara Dibdin <cdibdin@gmail.com>
2021-01-05 23:46:40 -0500

    first update

--- server/models/SQLTables.js
+++ server/models/SQLTables.js
@@ -7,7 +7,7 @@ const productsTable = `
     _id SERIAL,
     product_name VARCHAR NOT NULL,
     image_url VARCHAR NOT NULL,
-    google_url VARCHAR UNIQUE
+    google_url VARCHAR UNIQUE,
     PRIMARY KEY (_id)
   )`;
 
@@ -100,5 +100,3 @@ function insertIntoTable(queryString) {
 // insertIntoTable(lowestDailyPriceInsert);
 // insertIntoTable(usersInsert);
 // insertIntoTable(sessionsInsert);
-
-
